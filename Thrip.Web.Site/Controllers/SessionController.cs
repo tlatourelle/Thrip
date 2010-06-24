8a78539632b2a2153ee67d93193d619b6ad6f661
J Wynia <j@wynia.org>
2010-06-24 13:09:06 -0500

    Snapshot

---
+++ Thrip.Web.Site/Controllers/SessionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Thrip.Web.Site.Controllers
+{
+    public class SessionController : BaseController
+    {
+        //
+        // GET: /Session/
+
+        public ActionResult List(Guid id)
+        {
+            //id is the conference id
+            var _conference = Datacon
+            return View();
+        }
+
+    }
+}
